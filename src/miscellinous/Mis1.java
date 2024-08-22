5ccd36bf4f3360250df1d4da84acf97d170324c7
yogee <yogee@DESKTOP-UR78PNQ>
2024-08-22 20:44:51 +0530

    I have made the changes in Miscellaniuous please check in pull request.

--- src/miscellinous/Mis1.java
+++ src/miscellinous/Mis1.java
@@ -5,6 +5,8 @@ public class Mis1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("I have made a change here");
+		//Please check and confirm if it works
 	}
 
 }
